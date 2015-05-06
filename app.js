21093a9ddb69b82797af89d00ba118963f9ca564
Mohammed Siddeeq <msiddeeq15@gmail.com>
2015-05-05 21:00:53 -0400

    initial

---
+++ app.js
@@ -0,0 +1,15 @@
+
+var express = require('express');
+var app = express();
+
+app.set('port', (process.env.PORT || 5000));
+app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/node_modules'));
+
+app.get('/', function(request, response) {
+  response.sendfile('./public/html/core.html');
+});
+
+app.listen(app.get('port'), function() {
+  console.log("Node app is running at localhost:" + app.get('port'));
+});
---
+++ public/js/core.js
@@ -0,0 +1,7 @@
+define(function (require) {
+	'use strict';
+
+	var contacts = require('jsmodules/contact.js');
+
+	require('angular');
+});
